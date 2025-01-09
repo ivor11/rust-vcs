34bca900a7729fc34d7fc4a90ae58dc5aa63afd4
Ivor D'Souza <ivor2048@outlook.com>
2025-01-09 23:23:00 +0000

    vcs init

---
+++ src/init.rs
@@ -0,0 +1,8 @@
+use std::fs;
+use std::io::Error;
+
+// struct Repository {}
+
+pub fn init() -> Result<(), Error> {
+    fs::create_dir_all(".rust-vcs/commits").and_then(|_| fs::write(".rust-vcs/index", ""))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::{Parser, Subcommand};
+use std::io::ErrorKind;
 
-/// Simple VCS system
+mod init;
+
+/// Simple Version Control System
 #[derive(Parser, Debug)]
 #[command(name = "rust-vcs", version, about, long_about = None)]
 struct Args {
@@ -11,17 +14,37 @@ struct Args {
 #[derive(Clone, Debug, Subcommand)]
 enum Command {
     Init,
-    Commit {        
+    Add {
+        /// Paths to add to staging
+        path: Vec<String>,
+    },
+    Commit {
         /// Commit message
         #[arg(short, long)]
         message: String,
-    }
+    },
+    Checkout {
+        /// Commit to checkout to
+        commit: String,
+    },
 }
 
 fn main() {
     let args = Args::parse();
     match args.command {
-        Command::Init => println!("init the repo"),
-        Command::Commit { message } => print!("Commit with message {}", message)
+        Command::Init => match init::init() {
+            Ok(_) => println!("Initialzed VCS!"),
+            Err(err) => match err.kind() {
+                ErrorKind::AlreadyExists => eprint!("VCS already initialized!"),
+                _ => eprint!("Failed to initialize VCS"),
+            },
+        },
+        Command::Commit { message } => print!("Commit with message {}", message),
+        Command::Add { path } => {
+            for p in path {
+                println!("{}", p);
+            }
+        }
+        Command::Checkout { .. } => todo!(),
     }
 }
